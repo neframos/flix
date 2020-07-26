07adc793afa0c7ecaf4783e345ba8406d9d0b30f
neframos <mail@neframos.com>
2020-07-25 20:06:03 -0500

    updated gem file

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
+gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use sqlite3 as the database for Active Record
 
 group :development, :test do
@@ -61,3 +61,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem "aws-sdk-s3", require: false
+gem "actionpack", ">= 6.0.3.2"
+gem "rack", ">= 2.2.3"
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  resources :genres
-  resources :users
-  root 'movies#index'
-
-  resources :movies do
-    resources :reviews
-    resources :favorites, only: [:create, :destroy]
-  end
-  resource :session, only: %i[new create destroy]
-  get 'signup' => 'users#new'
-  get 'signin' => 'sessions#new'
-  get 'movies/filter/:filter' => 'movies#index'
-  get 'movies/filter/:filter' => 'movies#index', as: :filtered_movies
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
